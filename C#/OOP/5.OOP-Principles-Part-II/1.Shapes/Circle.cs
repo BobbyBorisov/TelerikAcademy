b311e0b76ecafcbe5a67548471d15a2324ec4f56
BobbyBorisov <borislav.borisov.tues@gmail.com>
2013-04-03 17:47:51 +0300

    Update Circle.cs

--- C#/OOP/5.OOP-Principles-Part-II/1.Shapes/Circle.cs
+++ C#/OOP/5.OOP-Principles-Part-II/1.Shapes/Circle.cs
@@ -9,12 +9,9 @@ namespace _1_Task
     class Circle: Shape
     {
 
-        public Circle(double height, double width)
-            :base(height,width)
-        {
-            this.Height = this.Width;
-        }
-
+        public Circle(double radius)
+            :base(radius,radius) {  }
+       
         public override double CalculateSurface()
         {
             return (Math.PI * (Height / 2) * (Height / 2));
