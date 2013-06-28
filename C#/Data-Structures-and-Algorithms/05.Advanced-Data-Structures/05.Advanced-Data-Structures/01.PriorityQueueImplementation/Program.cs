c0f6f407acdd6559f38bdc43989b0b36d9619b15
BobbyBorisov <borislav.borisov.tues@gmail.com>
2013-06-28 23:28:27 +0300

    Upload Advanced Data Structures(PowerCollection)

---
+++ C#/Data-Structures-and-Algorithms/05.Advanced-Data-Structures/05.Advanced-Data-Structures/01.PriorityQueueImplementation/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace _01.PriorityQueueImplementation
+{
+    class Program
+    {
+        static void Main()
+        {
+            PriorityQueue<int> heap = new PriorityQueue<int>(2);
+            heap.Enqueue(1);
+            heap.Enqueue(21);
+            heap.Enqueue(3);
+            heap.Enqueue(6);
+            Console.WriteLine(heap.Dequeue());
+            heap.Enqueue(31);
+            heap.Enqueue(5);
+            Console.WriteLine("ToString elements");
+            Console.WriteLine(heap.ToString());
+            Console.WriteLine("Dequeue elements");
+            while (heap.Count > 0)
+            {
+                Console.Write("{0}, ", heap.Dequeue());
+            }
+        }
+    }
+}
